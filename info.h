1d303d6910125c5abf91f765502b39db92831b0a
Giacca01 <fgiacardi01@gmail.com>
2021-12-08 17:23:19 +0100

    08/12/2021: Macros for reward and init transaction added. Formal definition of IPC structures. Minor changes to documentation

--- info.h
+++ info.h
@@ -7,6 +7,7 @@
 #include <time.h>
 
 #define SO_REGISTRY_SIZE 200
+// Right now it's not reentrant, we should modify it
 #define EXIT_ON_ERROR if (errno) {			\
 		fprintf(stderr),			\
 		"%d: pid %ld; errno: %d (%s)\n",  	\
@@ -17,8 +18,16 @@
 		exit(EXIT_FAILURE);			\
 }							\
 
+// sviluppare meglio: come affrontare il caso in cui SO_REGISTRY_SIZE % 3 != 0
+#define REG_PARTITION_SIZE (SO_REGISTRY_SIZE / 3) // Modificato
+#define REWARD_TRANSACTION -1 // Modificato
+#define INIT_TRANSACTION -1 // Modificato
+#define REG_PARTITION_COUNT 3 // Modificato
 
-struct transaction {
+typedef enum{TERMINATED = 0, ACTIVE} States; // Modificato
+
+// Nella documentazione fare disegno di sta roba
+typedef struct {
 	// meglio mettere la struct e non il singolo campo
 	// così non ci sono rischi di portablità
 	struct timespec timestamp;
@@ -29,19 +38,34 @@ struct transaction {
 	
 } Transaction;
 
-typedef struct transactionlist {
-	Transaction trans;
+typedef struct {
+	// puntatore per evitare troppe copie di struct per valore
+	Transaction * trans; // Modificato
 	Transaction * nextTrans;
 } TransactionList;
 
-struct block {
+typedef struct {
 	int bIndex;
 	// dato che la dimensione è variaile conviene fare una lista
 	TransactionList * transList;
 } Block;
 
-struct register{
+typedef struct{
 	int nBlocks;
-	// Vettore allocato dinamicamente??
-	Block blockList[SO_REGISTRY_SIZE];
+	// Vettore allocato dinamicamente?? Potrebbe semplificare il caso in cui
+	// SO_REGISTRY_SIZE % 3 != 0
+	// vettore di puntatori, altrimenti si fanno troppe copie di struct per valore
+	Block * blockList[REG_PARTITION_SIZE]; // modficato
 } Register;
+
+typedef struct procList {
+	long int procId;
+	States procState; // Modificato
+	procList * next;
+} ProcessesList;
+
+typedef struct tplist {
+	long int procId;
+	int msgId;
+	tplist * next;
+} TransactionPoolList;
