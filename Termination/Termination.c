4c92e2564bf4781cb83c6da3b2b1ac7be4f0db1b
Giacca01 <fgiacardi01@gmail.com>
2021-12-10 14:42:43 +0100

    Master version 0.15: Partial implementation of IPC facilitites deallocation procedure. Changes to the IPC facilities definition (see info.h and Notes.txt)

--- Termination/Termination.c
+++ Termination/Termination.c
@@ -1,6 +1,5 @@
 #include "../info.h"
 #include <string.h>
-
 /*
 	TODO:
 		-set simulation timer: Ok
@@ -24,74 +23,88 @@ void endOfSimulation(int);
 void printBudget();
 void deallocateFacilities();
 
-int main(){
+/*** IPC Pointers ***/
+
+// each partition is an array of registers
+Register regPtrs[REG_PARTITION_COUNT]; // to be loaded by master
+ProcListElem usersList[SO_USER_NUM];
+ProcListElem nodesList[SO_NODES_NUM];
+
+FriendsList processesFriends[SO_NODES_NUM]; // Ogni nodo ha una lista di amici
+TPElement tpList[SO_NODES_NUM];				// Ogni processo nodo ha un transaction pool
+
+/*** End of IPC Pointers ***/
+
+int main()
+{
 	// to be read from file
 	int SO_SIM_SEC = 5;
-	sigset_t set = 0;
+	sigset_t set;
 	int fullRegister = 1; // do a boolean datatype
 	int noTerminated = 0;
-	
-	/*** IPC Pointers ***/
-	
-	Register * regPtrs[REG_PARTITION_COUNT]; // to be loaded by master
-	ProcessesList * usersList = NULL;
-	ProcessesList * nodesList = NULL;
-	
-	ProcessesList ** processesFriends = NULL;
-	TransactionPoolList * tpList = NULL;
-	
-	/*** End of IPC Pointers ***/
-	
+
 	struct sigaction act;
-	
+
 	// check errors
 	alarm(SO_SIM_SEC);
-	
-	
-	if (sifillset(&set) == -1){
+
+	if (sifillset(&set) == -1)
+	{
 		EXIT_ON_ERROR;
-	} else {
+	}
+	else
+	{
 		act.sa_handler = endOfSimulation;
 		act.sa_mask = set;
-		if (sigaction(SIGALRM, act, NULL) == -1) {
+		if (sigaction(SIGALRM, act, NULL) == -1)
+		{
 			EXIT_ON_ERROR;
-		} else {
-			
-			if (sigaction(SIGUSR1, act, NULL) == -1) {
+		}
+		else
+		{
+
+			if (sigaction(SIGUSR1, act, NULL) == -1)
+			{
 				EXIT_ON_ERROR;
-			} else {
-				// check if register is full: in that case it must 
+			}
+			else
+			{
+				// check if register is full: in that case it must
 				// signal itself ? No
 				// this should be inserted in the master lifecycle
 				fullRegister = 1;
-				for (int i = 0; i < 3 && fullRegister; i++){
-					if (shmPtrs[i].noBlocks < SO_REGISTER-SIZE / 3)
-						fullRegister = 0;	
+				for (int i = 0; i < 3 && fullRegister; i++)
+				{
+					if (regPtrs[i]->nBlocks < SO_REGISTER - SIZE / 3)
+						fullRegister = 0;
 				}
-				
-				if (fullRegister) {
+
+				if (fullRegister)
+				{
 					endOfSimulation(SIGUSR1);
 				}
-				
 			}
-			
-			
 		}
 	}
+
+	exit(EXIT_SUCCESS);
 }
 
-void endOfSimulation(int sig){ // IT MUST BE REENTRANT!!!!
+void endOfSimulation(int sig)
+{ // IT MUST BE REENTRANT!!!!
 	// Notify children
 	// sends termination signal to all the processes
 	// that are part of the master's group (in this case we
 	// reach every children with just one system call).
 	// how to check if everyone was signaled (it returns true even if
 	// only one signal was sent)
-	char * terminationMessage = NULL;
+	char *terminationMessage = NULL;
 	int ret = -1;
-	
+	char *aus;
+
 	// viene inviato anche al master stesso ? In teoria no
-	if (kill(0, SIGUSR1) == 0) {
+	if (kill(0, SIGUSR1) == 0)
+	{
 		// wait for children
 		// dovremmo aspettare solo la ricezione del segnale di terminazione????
 		// mettere nell'handler
@@ -99,124 +112,141 @@ void endOfSimulation(int sig){ // IT MUST BE REENTRANT!!!!
 		// terminati correttamente ? Sarebbe complicato
 		// meglio inserire nel figlio un meccanismo che tenta più volte la stampa
 		// in caso di errore
-		while (wait(NULL) != -1);
-		
+		while (wait(NULL) != -1)
+			;
+
 		// print report: we use the write system call: slower, but async-signal-safe
 		// termination reason
-		if (sig == SIGALARM)
+		if (sig == SIGALRM)
 			aus = "Termination reason: end of simulation.\n";
 		else
 			aus = "Termination reason: register book is full.\n";
-		
+
 		// Users and nodes budgets
 		printBudget();
-		
+
+		/*Per la stampa degli errori non si può usare perror, perchè non è elencata* tra la funzioni signal*/
+
 		// processes terminated before end of simulation
 		ret = sprintf(terminationMessage, "Processes terminated before end of simulation: %d\n", noTerminated);
 		if (ret <= 0)
 			write(STDERR_FILENO, "Master: sprintf failed to format process count's string. Error: ");
-		
+
 		// Blocks in register
 		ret = sprintf(aus, "There are %d blocks in the register.\n", shmPtrs[0].nBlocks + nBlocks[1].nBlocks + nBlocks[2].nBlocks);
 		if (ret <= 0)
 			write(STDERR_FILENO, "Master: sprintf failed to format number of blocks' string. Error: "); // error handling
-		
+
 		ret = write(STDOUT_FILENO, terminationMessage, strlen(terminationMessage));
 		if (ret == -1)
 			write(STDERR_FILENO, "Master: failed to write termination message. Error: "); // error handling
-		
-		
+
 		// deallocate facilities
 		deallocateFacilities();
-	} else {
+	}
+	else
+	{
 		// implement a retry mechanism
 	}
 }
 
-void printBudget(){ // Vedere se si possa sostituire la write con printf
-	Register * reg = NULL; 
-	long int pid = -2; // -1 è usato per rappresentare la transazione di reward e di init
-	ProcessesList * usr = NULL; // non sono da deallocare, altrimenti cancelli la lista di processi
-	ProcessesList * node = NULL;
-	Transaction * transPtr = NULL;
-	long float balance = 0; // leggendo la transazione di inizializzazione (i.e. la prima indirizzata la processo)
-				// verrà inizializzata a SO_BUDGET_INIT
-	char * balanceString = NULL;
+void printBudget()
+{ // Vedere se si possa sostituire la write con printf
+	Register *reg = NULL;
+	long int pid = -2;		   // -1 è usato per rappresentare la transazione di reward e di init
+	ProcessesList *usr = NULL; // non sono da deallocare, altrimenti cancelli la lista di processi
+	ProcessesList *node = NULL;
+	Transaction *transPtr = NULL;
+	float balance = 0; // leggendo la transazione di inizializzazione (i.e. la prima indirizzata la processo)
+					   // verrà inizializzata a SO_BUDGET_INIT
+	char *balanceString = NULL;
 	int ret = 0;
 	int i = 0;
 	int j = 0;
-	TransactionList * tList = NULL;
-	
-	
+	TransactionList *tList = NULL;
+
 	// Compute balance for users
 	ret = write(STDOUT_FILENO, "Master prints users' balances...\n", strlen("Master prints users' balances...\n"));
 	if (ret == -1)
 		write(STDERR_FILENO, "Master: failed to write operation's description. Error: ");
-	else {
+	else
+	{
 		usr = usersList;
 		// è un algoritmo di complessità elevata
 		// vedere se sia possibile ridurla
 		// for each user...
-		while (usr != NULL) {
+		while (usr != NULL)
+		{
 			pid = (long)(usr->procId);
 			balance = 0;
 			// we scan all the register's partitions...
-			for (i = 0; i < REG_PARTITION_COUNT; i++){
+			for (i = 0; i < REG_PARTITION_COUNT; i++)
+			{
 				aus = regPtrs[i];
 				// we scan every block in the partition
 				// and every transaction in it to compute the balance
-				for (j = 0; j < REG_PARTITION_SIZE; j++) {
+				for (j = 0; j < REG_PARTITION_SIZE; j++)
+				{
 					// necessary in order not to lose
 					// the transaction list pointer
 					tList = (blockList[i]->transList);
-					while (tList != NULL) {
+					while (tList != NULL)
+					{
 						transPtr = tList->trans;
 						if (transPtr->sender == pid)
 							balance -= transPtr->amountSend;
-						else (transPtr->receiver == pid)
-							balance += transPtr->amountSend;
+						else
+							(transPtr->receiver == pid)
+								balance += transPtr->amountSend;
 						tList++;
 					}
 				}
 			}
-			
+
 			ret = sprintf(balanceString, "The balance of the user of PID %ld is: %lf", pid, balance);
-			if (ret <= 0) {
+			if (ret <= 0)
+			{
 				if (write(STDOUT_FILENO, balanceString, strlen(balanceString)) == -1)
 					write(STDERR_FILENO, "Master: failed to write balance message for user. Error: ");
 			}
 			usr++;
 		}
 	}
-	
+
 	ret = write(STDOUT_FILENO, "Master prints nodes' balances...\n", strlen("Master prints nodes' balances...\n"));
 	if (ret == -1)
 		write(STDERR_FILENO, "Master: failed to write operation's description. Error: ");
-	else {
+	else
+	{
 		node = nodesList;
-		while (node != NULL) {
+		while (node != NULL)
+		{
 			pid = (long)(node->procId);
 			balance = 0;
-			for (i = 0; i < REG_PARTITION_COUNT; i++){
+			for (i = 0; i < REG_PARTITION_COUNT; i++)
+			{
 				aus = regPtrs[i];
 				// note that we start from one because there's no
 				// initialization blocks for nodes
-				for (j = 1; j < REG_PARTITION_SIZE; j++) {
+				for (j = 1; j < REG_PARTITION_SIZE; j++)
+				{
 					tList = (blockList[i]->transList);
-					while (tList != NULL) {
+					while (tList != NULL)
+					{
 						transPtr = tList->trans;
 						// non c'è il rischio di contare più volte le transazioni
 						/// perchè cerchiamo solo quella di reward e l'implementazione
 						// del nodo garantisce che c'è ne sia una sola per blocco
 						// serve testare
-						if (transPtr->sender == REWARD_TRANSACTION  && transPtr->receiver == pid)
+						if (transPtr->sender == REWARD_TRANSACTION && transPtr->receiver == pid)
 							balance += transPtr->amountSend;
 						tList++;
 					}
 				}
 			}
 			ret = sprintf(balanceString, "The balance of the node of PID %ld is: %lf", pid, balance);
-			if (ret <= 0) {
+			if (ret <= 0)
+			{
 				if (write(STDOUT_FILENO, balanceString, strlen(balanceString)) == -1)
 					write(STDERR_FILENO, "Master: failed to write balance message for node. Error: ");
 			}
@@ -225,5 +255,57 @@ void printBudget(){ // Vedere se si possa sostituire la write con printf
 	}
 }
 
-void deallocateFacilities(){
+void deallocateFacilities()
+{
+	// Ovviamente i processi figli dovranno scollegarsi
+	// dai segmenti e chiudere i loro riferimenti alla coda
+	// ed ai semafori prima di terminare
+
+	// Precondizione: tutti i processi figli si sono scollegati dai segmenti di memoria
+	// In generale, tutti i figli hanno chiuso i loro riferimenti alle facilities IPC
+
+	// Cosa fare se una delle system call usate per la deallocazione fallisce?
+	/*
+		L'idea è quella di implementare l'eliminazione di ogni facility in maniera indipendente
+		dalle altre (i.e. l'eliminazione della n + 1 viene effettuata anche se quella dell'n-esima è fallita)
+		ma di non implementare un meccanismo tale per cui si tenta ripetutamente di eliminare
+		la facility n-esima qualora una delle system call coinvolte fallisca
+	*/
+
+	/*
+		TODO:
+			-dellocate registers partition: OK
+			-deallocate users and nodes lists (serve fare la malloc o basta deallocare il segmento???)
+			-deallocate transaction pools list' shared memory segment
+			-deallocate friens' shared memory segment
+			-deallocate transaction pools' message queues
+			-deallocate semaphores
+	*/
+
+	char *aus = NULL;
+	int msgLength = 0;
+
+	// Deallocating register's partitions
+	for (int i = 0; i < regPtrs; i++)
+	{
+		if (shmdt(regPtrs[i]) == -1)
+		{
+			msgLength = sprintf(aus, "Master: failed to detach from register partition number %d.", (i + 1));
+			if (msgLength < 0)
+				write(STDERR_FILENO, "Master: failed to format output message in IPC deallocation.", strlen("Master: failed to format output message in IPC deallocation."));
+			else
+				write(STDERR_FILENO, aus, msgLength); // by doing this we avoid calling strlength
+		}
+		else
+		{
+			if (smhctl(regPtrs[i], IPC_RMID, NULL) == -1)
+			{
+				msgLength = sprintf(aus, "Master: failed to remove register partition number %d.", (i + 1));
+				if (msgLength < 0)
+					write(STDERR_FILENO, "Master: failed to format output message in IPC deallocation.", strlen("Master: failed to format output message in IPC deallocation."));
+				else
+					write(STDERR_FILENO, aus, msgLength);
+			}
+		}
+	}
 }
---
+++ info.h
@@ -0,0 +1,89 @@
+// ATTENZIONE!!!! Quella che segue deve SEMPRE ESSERE LA PRIMA DEFINE
+#define _GNU_SOURCE
+#include <stdio.h>
+#include <unistd.h>
+#include <signal.h>
+#include <sys/types.h>
+#include <time.h>
+#include <string.h>
+#include <errno.h>
+#include <stdlib.h>
+
+#define SO_REGISTRY_SIZE 200
+// Right now it's not reentrant, we should modify it
+#define EXIT_ON_ERROR                        \
+    if (errno)                               \
+    {                                        \
+        fprintf(stderr),                     \
+            "%d: pid %ld; errno: %d (%s)\n", \
+            __LINE__,                        \
+            (long)getpid(),                  \
+            errno,                           \
+            strerror(errno());               \
+        exit(EXIT_FAILURE);                  \
+    }
+
+// sviluppare meglio: come affrontare il caso in cui SO_REGISTRY_SIZE % 3 != 0
+#define REG_PARTITION_SIZE (SO_REGISTRY_SIZE / 3) 
+#define REWARD_TRANSACTION -1                     
+#define INIT_TRANSACTION -1                       
+#define REG_PARTITION_COUNT 3                     
+#define SO_BLOCK_SIZE 10                          // Modificato 10/12/2021
+#define SO_USER_NUM 10                            // Modificato 10/12/2021
+#define SO_NODES_NUM 7                            // Modificato 10/12/2021
+
+typedef enum
+{
+    TERMINATED = 0,
+    ACTIVE
+} States; 
+
+// Nella documentazione fare disegno di sta roba
+typedef struct
+{
+    // meglio mettere la struct e non il singolo campo
+    // così non ci sono rischi di portablità
+    struct timespec timestamp;
+    long int sender;    // Sender's PID
+    long int receiver; // Receiver's PID
+    float amountSend;
+    float reward;
+
+} Transaction;
+
+// Each block is a list of transactions
+typedef struct // Modificato 10/12/2021
+{
+    int bIndex;
+    // SO_BLOCK_SIZE è da leggere da file???
+    Transaction transList[SO_BLOCK_SIZE];
+} Block;
+
+// Each register is a group of blocks
+typedef struct // Modificato 10/12/2021
+{
+    int nBlocks;
+    // Vettore allocato dinamicamente?? Potrebbe semplificare il caso in cui
+    // SO_REGISTRY_SIZE % 3 != 0
+    // NOPE, non si potrebbe condividere
+    Block blockList[REG_PARTITION_SIZE]; // modficato
+} Register;
+
+typedef struct // Modificato 10/12/2021
+{
+    long int procId;
+    States procState; 
+} ProcListElem;
+
+// Il singolo elemento della lista degli amici è una coppia
+// -(PID del processo a cui è riferita, lista di pid e stato degli amici)
+typedef struct { // Modificato 10/12/2021
+    long int ownerId;
+    ProcListElem friends;
+} FriendsList;
+
+typedef struct // Modificato 10/12/2021
+{
+    long int procId; // è necessario???
+    int msgQId;
+} TPElement;
